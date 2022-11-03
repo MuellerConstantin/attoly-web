2dad91d0fec3b6d78fe48ddcf1c986c2a71c807b
Constantin Müller <32311121+0x1C1B@users.noreply.github.com>
2022-11-03 13:54:21 +0100

    feat: add basic app layout
    Adds a basic corporate design compliant app layout.

---
+++ src/components/organisms/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <div className="text-center bg-orange-500 text-gray-800 dark:text-white">
+      <div className="text-center p-4 bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-white">
+        Made with&nbsp;
+        <span className="text-red-500">&#x2764;</span>
+        &nbsp;by&nbsp;
+        <a
+          className="text-sky-500 font-semibold hover:brightness-110 hover:underline"
+          href="https://github.com/0x1c1b"
+          target="_blank"
+          rel="noreferrer"
+        >
+          0x1C1B
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/templates/StackTemplate.jsx
+++ src/components/templates/StackTemplate.jsx
@@ -1,9 +1,16 @@
+import Navbar from "../organisms/Navbar";
+import Footer from "../organisms/Footer";
+
 export default function StackTemplate({ children }) {
   return (
     <div className="h-screen flex flex-col">
-      <header />
+      <header>
+        <Navbar />
+      </header>
       <main className="grow">{children}</main>
-      <footer />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
