94786e18ae044ad67faa84289ade7e933042d9d9
Constantin Müller <info@mueller-constantin.de>
2026-02-02 08:34:17 +0100

    feat: add client side auth guard

--- src/app/[locale]/settings/layout.tsx
+++ src/app/[locale]/settings/layout.tsx
@@ -5,6 +5,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth-options";
 import { ListBox, ListBoxItem } from "@/components/atoms/ListBox";
 import { redirect } from "next/navigation";
+import { AuthGuard } from "@/components/organisms/AuthProvider";
 
 export default async function SettingsLayout({
   children,
@@ -19,38 +20,40 @@ export default async function SettingsLayout({
   }
 
   return (
-    <StackTemplate>
-      <div className="mx-auto my-8 flex w-full max-w-[80rem] grow flex-col gap-4 gap-12 p-4 md:flex-row">
-        <div className="flex w-full shrink-0 flex-col gap-4 md:w-1/3 xl:w-1/4">
-          <div className="flex items-center gap-4 rounded-md border border-slate-200 bg-orange-500 p-4 text-white">
-            <UserCircle className="h-10 w-10" />
-            <span className="truncate text-sm font-bold">
-              {session!.user.id}
-            </span>
+    <AuthGuard>
+      <StackTemplate>
+        <div className="mx-auto my-8 flex w-full max-w-[80rem] grow flex-col gap-4 gap-12 p-4 md:flex-row">
+          <div className="flex w-full shrink-0 flex-col gap-4 md:w-1/3 xl:w-1/4">
+            <div className="flex items-center gap-4 rounded-md border border-slate-200 bg-orange-500 p-4 text-white">
+              <UserCircle className="h-10 w-10" />
+              <span className="truncate text-sm font-bold">
+                {session!.user.id}
+              </span>
+            </div>
+            <ListBox>
+              <ListBoxItem href="/settings/account">
+                <div className="flex items-center gap-2">
+                  <User className="h-5 w-5" />
+                  <span>{t("account")}</span>
+                </div>
+              </ListBoxItem>
+              <ListBoxItem href="/settings/billing">
+                <div className="flex items-center gap-2">
+                  <CreditCard className="h-5 w-5" />
+                  <span>{t("billing")}</span>
+                </div>
+              </ListBoxItem>
+              <ListBoxItem href="/settings/shortcuts">
+                <div className="flex items-center gap-2">
+                  <LinkIcon className="h-5 w-5" />
+                  <span>{t("shortLinks")}</span>
+                </div>
+              </ListBoxItem>
+            </ListBox>
           </div>
-          <ListBox>
-            <ListBoxItem href="/settings/account">
-              <div className="flex items-center gap-2">
-                <User className="h-5 w-5" />
-                <span>{t("account")}</span>
-              </div>
-            </ListBoxItem>
-            <ListBoxItem href="/settings/billing">
-              <div className="flex items-center gap-2">
-                <CreditCard className="h-5 w-5" />
-                <span>{t("billing")}</span>
-              </div>
-            </ListBoxItem>
-            <ListBoxItem href="/settings/shortcuts">
-              <div className="flex items-center gap-2">
-                <LinkIcon className="h-5 w-5" />
-                <span>{t("shortLinks")}</span>
-              </div>
-            </ListBoxItem>
-          </ListBox>
+          <div className="flex grow flex-col">{children}</div>
         </div>
-        <div className="flex grow flex-col">{children}</div>
-      </div>
-    </StackTemplate>
+      </StackTemplate>
+    </AuthGuard>
   );
 }
--- src/components/organisms/AuthProvider.tsx
+++ src/components/organisms/AuthProvider.tsx
@@ -1,11 +1,36 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export function AuthProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider refetchInterval={60} refetchOnWindowFocus={true}>
+      {children}
+    </SessionProvider>
+  );
+}
+
+export function AuthGuard({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      signOut({ callbackUrl: "/signin" });
+    }
+  }, [status]);
+
+  if (status === "authenticated") {
+    return <>{children}</>;
+  }
+
+  return null;
 }
